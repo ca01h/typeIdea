c7431aecf062e71d0152df4a5f3b3ddf4d851178
caoyihuai2 <cc511965738@gmail.com>
2018-05-06 22:31:17 +0800

    完成Function-View升级为Class-Base-View

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,94 @@ from __future__ import unicode_literals
 
 from django.core.paginator import Paginator, EmptyPage
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from blog.models import Post, Tag, Category
 from config.models import SlideBar
 from comment.models import Comment
 
+
 # Create your views here.
+
+
+class CommonMixin(object):
+    def get_context_data(self, **kwargs):
+
+        categories = Category.objects.filter(status=1)  # TODO: fix magic number
+        """
+        1.Hit the DB
+        nav_cates = categories.filter(is_nav=True)
+        cates = categories.filter(is_nav=False)
+
+        2.
+        nav_cates = [cate for cate in categories if cate.is_nav]
+        cates = [cate for cate in categories if not cate.is_nav]
+        """
+
+        nav_cates = []
+        cates = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_cates.append(cate)
+            else:
+                cates.append(cate)
+
+        slidebars = SlideBar.objects.filter(status=1)
+
+        recently_posts = Post.objects.filter(status=1)[:10]
+        # hot_posts = Post.objects.filter(status=1).order_by('views')[:10]
+        recently_comments = Comment.objects.filter(status=1)[:10]
+
+        extra_context = {
+            'nav_cates': nav_cates,
+            'cates': cates,
+            'slidebars': slidebars,
+            'recently_posts': recently_posts,
+            'recently_comments': recently_comments,
+
+        }
+
+        return super(CommonMixin, self).get_context_data(**extra_context)
+
+
+class BasePostView(CommonMixin, ListView):
+    model = Post
+    template_name = 'blog/list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+
+class IndexView(BasePostView):
+    pass
+
+
+class CategoryView(BasePostView):
+    def get_queryset(self):
+        qs = super(CategoryView, self).get_queryset()
+        # 从URL中获取category_id值
+        cate_id = self.kwargs.get('category_id')
+        qs = qs.filter(category_id=cate_id)
+        return qs
+
+
+class TagView(BasePostView):
+    def get_queryset(self):
+        tag_id = self.kwargs.get('tag_id')
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            return []
+
+        posts = tag.posts.all()
+        return posts
+
+
+class PostView(CommonMixin, DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
 
 def get_common_context():
     categories = Category.objects.filter(status=1)  # TODO: fix magic number
@@ -45,54 +127,3 @@ def get_common_context():
 
     }
     return context
-
-
-
-def post_list(request, tag_id=None, category_id=None):
-
-    # 分页
-    page_size = 3
-    page = request.GET.get('page', 1)
-    try:
-        page = int(page)
-    except TypeError:
-        page = 1
-
-    queryset = Post.objects.all()
-    if category_id:
-        # 分类页面
-        queryset = queryset.filter(category_id=category_id)
-    elif tag_id:
-        # 标签页面
-        try:
-            tag = Tag.objects.all()
-        except Tag.DoesNotExist:
-            queryset = []
-        else:
-            queryset = queryset.filter(tag_id=tag_id)
-
-    paginator = Paginator(queryset, page_size)
-    try:
-        posts = paginator.page(page)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    context = {
-        'posts': posts,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-    return render(request, 'blog/list.html', context=context)
-
-
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404('post does not exist')
-    context = {
-        'post': post,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-    return render(request, 'blog/detail.html', context=context)
--- typeIdea/urls.py
+++ typeIdea/urls.py
@@ -17,16 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .custom_site import custom_site
-from blog.views import post_detail, post_list
+from blog.views import IndexView, CategoryView, TagView, PostView
 
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
     url(r'^cus_admin/', custom_site.urls),
 
-    url(r'^$', post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    url(r'^post/(?P<post_id>\d+)/$', post_detail),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag'),
+    url(r'^post/(?P<pk>\d+)/$', PostView.as_view(), name='detail'),
 
 ]
